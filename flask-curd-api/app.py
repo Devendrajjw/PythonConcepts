7d78abd5f2b7fd8bc76bb3154721051810c00a8f
Devendra <devendrajjw@gmail.com>
2024-05-29 01:52:55 +0530

    new files

--- flask-curd-api/app.py
+++ flask-curd-api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
 # from os import environ
 
 app = Flask(__name__)  # read why it is used
@@ -45,6 +46,23 @@ def create_user():
         return make_response(jsonify({'message': 'error creating user'}), 500)
 
 
+# update a user
+@app.route('/users101/<int:id>', methods=['PUT'])
+def update_user(id):
+    try:
+        user = User.query.filter_by(id=id).first()
+        if user:
+            data = request.get_json()
+            user.username = data['username']
+            user.email = data['email']
+            db.session.commit()
+            return make_response(jsonify({'message': 'user updated', 'updated': user.json()}), 200)
+        return make_response(jsonify({'message': 'user not found with given id'}), 404)
+    except Exception as e:
+        print(e)
+        return make_response(jsonify({'message': 'error while updating user'}))
+
+
 # get all users
 @app.route('/users101', methods=['GET'])
 def get_users():
@@ -69,28 +87,25 @@ def get_user(id):
         return make_response(jsonify({'message': 'error getting user'}), 500)
 
 
-# update a user
-@app.route('/users101/<int:id>', methods=['PUT'])
-def update_user(id):
-    try:
-        user = User.query.filter_by(id=id).first()
-        if user:
-            data = request.get_json()
-            user.username = data['username']
-            user.email = data['email']
-            db.session.commit()
-            return make_response(jsonify({'message': 'user updated', 'updated': user.json()}), 200)
-        return make_response(jsonify({'message': 'user not found with given id'}), 404)
-    except Exception as e:
-        print(e)
-        return make_response(jsonify({'message': 'error while updating user'}))
-
-
 # delete a user
 @app.route('/users101/<int:id>', methods=['DELETE'])
 def delete_user(id):
     try:
         user = User.query.filter_by(id=id).first()
+        if user:
+            db.session.delete(user)
+            db.session.commit()
+            return make_response(jsonify({'message': 'user deleted', 'deleted': user.json()}), 200)
+        return make_response(jsonify({'message': 'user not found'}), 400)
+    except Exception as e:
+        print(e)
+        return make_response(jsonify({'message': 'error while delete'}))
+
+
+@app.route('/users101/<int:id>/<string:username>', methods=['DELETE'])
+def delete_user_by_id_name(id, username):
+    try:
+        user = User.query.filter(and_(User.id == id, User.username == username)).first()
         if user:
             db.session.delete(user)
             db.session.commit()
