9015e29bd111835bb943a40ed78f9e029e6d1c9b
Devendra <devendrajjw@gmail.com>
2024-07-03 23:46:18 +0530

    new files

---
+++ combination/targetSumcombination.py
@@ -0,0 +1,9 @@
+arr = [2, 3, 1, 5, 8, 9, 10, 4, 6]
+target = 10
+nl = []
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
+        if arr[i] + arr[j] == target:
+            nl.append((arr[i], arr[j]))
+print(nl)
+
